2088868ed75c88090324499dafad8a505bb86ff5
Romano Javier Alexis <alexis_b13@live.com.ar>
2022-07-04 09:46:06 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+require("electron-reload")(__dirname);
+
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadFile("src/view/index.html");
+}
+
+app.allowRendererProcessReuse = true;
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
