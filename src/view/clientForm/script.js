3f74274e319903d721f3566007dec1383d48f29e
Romano Javier Alexis <alexis_b13@live.com.ar>
2022-07-05 22:14:48 -0300

    Client Form, syntax, styles and validations complete

--- src/view/clientForm/script.js
+++ src/view/clientForm/script.js
@@ -1,86 +1,203 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+/* ----------------------------
 
-// Show input error message
-function showError(input, message) {
-  const formControl = input.parentElement;
-  formControl.className = 'form-control error';
-  const small = formControl.querySelector('small');
-  small.innerText = message;
+	CustomValidation prototype
+
+	- Keeps track of the list of invalidity messages for this input
+	- Keeps track of what validity checks need to be performed for this input
+	- Performs the validity checks and sends feedback to the front end
+
+---------------------------- */
+
+function CustomValidation(input) {
+  this.invalidities = [];
+  this.validityChecks = [];
+
+  //add reference to the input node
+  this.inputNode = input;
+
+  //trigger method to attach the listener
+  this.registerListener();
 }
 
-// Show success outline
-function showSuccess(input) {
-  const formControl = input.parentElement;
-  formControl.className = 'form-control success';
-}
+CustomValidation.prototype = {
+  addInvalidity: function (message) {
+    this.invalidities.push(message);
+  },
+  getInvalidities: function () {
+    return this.invalidities.join(". \n");
+  },
+  checkValidity: function (input) {
+    for (var i = 0; i < this.validityChecks.length; i++) {
+      const isInvalid = this.validityChecks[i].isInvalid(input);
+      if (isInvalid) {
+        this.addInvalidity(this.validityChecks[i].invalidityMessage);
+      }
 
-// Check email is valid
-function checkEmail(input) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (re.test(input.value.trim())) {
-    showSuccess(input);
-  } else {
-    showError(input, 'Email is not valid');
-  }
-}
+      const requirementElement = this.validityChecks[i].element;
 
-// Check required fields
-function checkRequired(inputArr) {
-  let isRequired = false;
-  inputArr.forEach(function(input) {
-    if (input.value.trim() === '') {
-      showError(input, `${getFieldName(input)} is required`);
-      isRequired = true;
+      if (requirementElement) {
+        if (isInvalid) {
+          requirementElement.classList.add("invalid");
+          requirementElement.classList.remove("valid");
+        } else {
+          requirementElement.classList.remove("invalid");
+          requirementElement.classList.add("valid");
+        }
+      } // end if requirementElement
+    } // end for
+  },
+  checkInput: function () {
+    // checkInput now encapsulated
+
+    this.inputNode.CustomValidation.invalidities = [];
+    this.checkValidity(this.inputNode);
+
+    if (
+      this.inputNode.CustomValidation.invalidities.length === 0 &&
+      this.inputNode.value !== ""
+    ) {
+      this.inputNode.setCustomValidity("");
     } else {
-      showSuccess(input);
+      var message = this.inputNode.CustomValidation.getInvalidities();
+      this.inputNode.setCustomValidity(message);
     }
-  });
+  },
+  registerListener: function () {
+    //register the listener here
 
-  return isRequired;
-}
+    var CustomValidation = this;
 
-// Check input length
-function checkLength(input, min, max) {
-  if (input.value.length < min) {
-    showError(
-      input,
-      `${getFieldName(input)} must be at least ${min} characters`
-    );
-  } else if (input.value.length > max) {
-    showError(
-      input,
-      `${getFieldName(input)} must be less than ${max} characters`
-    );
-  } else {
-    showSuccess(input);
+    this.inputNode.addEventListener("keyup", function () {
+      CustomValidation.checkInput();
+    });
+  },
+};
+
+/* ----------------------------
+
+	Validity Checks
+
+	The arrays of validity checks for each input
+	Comprised of three things
+		1. isInvalid() - the function to determine if the input fulfills a particular requirement
+		2. invalidityMessage - the error message to display if the field is invalid
+		3. element - The element that states the requirement
+
+---------------------------- */
+
+const dniValidityChecks = [
+  {
+    isInvalid: function (input) {
+      return input.value.length > 8;
+    },
+    invalidityMessage: "Un dni debe ser menor a los 9 digitos",
+    element: document.querySelector(
+      'label[for="dni"] .input-requirements li:nth-child(1)'
+    ),
+  },
+  {
+    isInvalid: function (input) {
+      const regex = /^[0-9]{7,8}$/;
+      const caracters = input.value;
+      const test = regex.test(caracters);
+      return test ? false : true;
+    },
+    invalidityMessage: "Un dni solo tiene numeros",
+    element: document.querySelector(
+      'label[for="dni"] .input-requirements li:nth-child(2)'
+    ),
+  },
+];
+
+const nameValidityChecks = [
+  {
+    isInvalid: function (input) {
+      const regex = /^[A-z]{4,30}$/;
+      const caracters = input.value;
+      const test = regex.test(caracters);
+      return test ? false : true;
+    },
+    invalidityMessage: "Un nombre solo contiene letras",
+    element: document.querySelector(
+      'label[for="name"] .input-requirements li:nth-child(1)'
+    ),
+  },
+];
+
+const surnameValidityChecks = [
+  {
+    isInvalid: function (input) {
+      const regex = /^[A-z]{4,30}$/;
+      const caracters = input.value;
+      const test = regex.test(caracters);
+      return test ? false : true;
+    },
+    invalidityMessage: "Un apellido solo contiene letras",
+    element: document.querySelector(
+      'label[for="surname"] .input-requirements li:nth-child(1)'
+    ),
+  },
+];
+
+const phoneNumberValidtyChecks = [
+  {
+    isInvalid: function (input) {
+      const regex = /^[0-9]{10,10}$/;
+      const caracters = input.value;
+      const test = regex.test(caracters);
+      return test ? false : true;
+    },
+    invalidityMessage: "Un numero de telefono solo contiene numeros",
+    element: document.querySelector(
+      'label[for="phoneNumber"] .input-requirements li:nth-child(1)'
+    ),
+  },
+];
+/* ----------------------------
+
+	Setup CustomValidation
+
+	Setup the CustomValidation prototype for each input
+	Also sets which array of validity checks to use for that input
+
+---------------------------- */
+
+const dniInput = document.getElementById("dni");
+const nameInput = document.getElementById("name");
+const surnameInput = document.getElementById("surname");
+const phoneNumberInput = document.getElementById("phoneNumber");
+
+dniInput.CustomValidation = new CustomValidation(dniInput);
+dniInput.CustomValidation.validityChecks = dniValidityChecks;
+
+nameInput.CustomValidation = new CustomValidation(nameInput);
+nameInput.CustomValidation.validityChecks = nameValidityChecks;
+
+surnameInput.CustomValidation = new CustomValidation(surnameInput);
+surnameInput.CustomValidation.validityChecks = surnameValidityChecks;
+
+phoneNumberInput.CustomValidation = new CustomValidation(phoneNumberInput);
+phoneNumberInput.CustomValidation.validityChecks = phoneNumberValidtyChecks;
+/* ----------------------------
+
+	Event Listeners
+
+---------------------------- */
+
+var inputs = document.querySelectorAll('input:not([type="submit"])');
+
+var submit = document.querySelector('button[type="submit"');
+var form = document.getElementById("saveClient");
+
+function validate() {
+  for (var i = 0; i < inputs.length; i++) {
+    inputs[i].CustomValidation.checkInput();
   }
 }
 
-// Check passwords match
-function checkPasswordsMatch(input1, input2) {
-  if (input1.value !== input2.value) {
-    showError(input2, 'Passwords do not match');
-  }
-}
-
-// Get fieldname
-function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
-
-// Event listeners
-form.addEventListener('submit', function(e) {
+submit.addEventListener("click", validate);
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-
-  if(checkRequired([username, email, password, password2])){
-    checkLength(username, 3, 15);
-    checkLength(password, 6, 25);
-    checkEmail(email);
-    checkPasswordsMatch(password, password2);
-  }
-
+  validate();
+  console.log("a");
 });
