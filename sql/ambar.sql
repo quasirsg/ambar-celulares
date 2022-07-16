f9e50c0498563bb7d518d250a8e530f7a4728468
Romano Javier Alexis <alexis_b13@live.com.ar>
2022-07-16 17:47:07 -0300

    sql

---
+++ sql/ambar.sql
@@ -0,0 +1,40 @@
+CREATE SCHEMA IF NOT EXISTS `ambar` DEFAULT CHARACTER SET utf8 ;
+
+CREATE TABLE IF NOT EXISTS `ambar`.`clients` (
+  `dni` INT UNSIGNED NOT NULL,
+  `name` VARCHAR(30) NOT NULL,
+  `surname` VARCHAR(30) NOT NULL,
+  `phone_number` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`dni`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `ambar`.`benefits` (
+  `idbenefits` INT NOT NULL AUTO_INCREMENT,
+  `dni` INT UNSIGNED NOT NULL,
+  `device` VARCHAR(30) NOT NULL,
+  `imei` VARCHAR(15) NOT NULL,
+  `description` VARCHAR(255) NOT NULL,
+  `replacements` VARCHAR(255) NULL,
+  `entry_date` INT NOT NULL,
+  `mount` INT NULL,
+  `retired` BOOLEAN,
+  `fixed` BOOLEAN,
+  PRIMARY KEY (`idbenefits`),
+  INDEX `fk_client_benefits_idx` (`dni` ASC) VISIBLE,
+  CONSTRAINT `fk_client_benefits`
+    FOREIGN KEY (`dni`)
+    REFERENCES `ambar`.`clients` (`dni`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+INSERT INTO `ambar`.`clients` (`dni`, `name`, `surname`, `phone_number`) VALUES (42523334, 'Alexis', 'Romano', '3813302319');
+
+SELECT dni FROM `ambar`.`clients`;
+
+INSERT INTO `ambar`.`benefits` (`idbenefits`, `dni`, `device`, `imei`, `description`,`replacements`, `entry_date`, `mount`,`retired`,`fixed`) 
+VALUES (DEFAULT, 42523334, 'g532m', '111111111111111','Problema en la batería', 'todos', 16072022, 3050,true,true);
+
+SELECT imei FROM `ambar`.`clients` c INNER JOIN `ambar`.`benefits` b ON 
+c.dni = b.dni;
