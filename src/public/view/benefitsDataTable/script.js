61177a68205603bb5dace68030b987e2cf9b68a9
Romano Javier Alexis <alexis_b13@live.com.ar>
2022-07-28 02:27:50 -0300

    Express integration + endpoint de prueba

--- src/public/view/benefitsDataTable/script.js
+++ src/public/view/benefitsDataTable/script.js
@@ -97,6 +97,8 @@ form.addEventListener("submit", async function (e) {
 
   validate();
   await reloadData();
+  const chave = await getChaves();
+  console.log(chave);
   const checksOfRetired = document.getElementsByClassName("checks_of_retired");
   const checksOfPaidOut = document.getElementsByClassName("checks_of_paid_out");
   const checksOfFixed = document.getElementsByClassName("checks_of_fixed");
