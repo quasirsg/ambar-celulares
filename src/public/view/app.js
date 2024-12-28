1463ba25b39d0e4229a21116c57b17d4de2d9da4
Oscar Nicolas Chaves <70153563+Nicolas2021@users.noreply.github.com>
2024-12-28 17:05:11 -0300

    Feat/rework-database-queries

--- src/public/view/app.js
+++ src/public/view/app.js
@@ -45,12 +45,16 @@ async function saveBenefit(benefit) {
  * @param {String} dni
  * @returns {BenefitObject}
  */
-async function getBenefits(dni, page, limit) {
-  return await main.getBenefits(dni, page, limit);
+async function getBenefits(dni, page, limit, filter) {
+  return await main.getBenefits(dni, page, limit, filter);
 }
 
-async function getAllBenefitsByDni(dni) {
+/* async function getAllBenefitsByDni(dni) {
   return await main.getAllBenefitsByDni(dni);
+} */
+
+async function getTotalBenefits(dni, filter) {
+  return await main.getTotalBenefits(dni, filter);
 }
 
 async function getPhoneBrands() {
