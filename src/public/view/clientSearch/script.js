1086ed91fc335394ea500dcdec9f7df48854c794
Oscar Nicolas Chaves <70153563+Nicolas2021@users.noreply.github.com>
2025-01-10 23:19:18 -0300

    Feat/add-modal-and-logic-to-charge-the-table

--- src/public/view/clientSearch/script.js
+++ src/public/view/clientSearch/script.js
@@ -5,25 +5,47 @@ const form = document.getElementById("search-form");
 
 let dni;
 
-async function clientInfo(dni) {
-    if (verifyIsValidDni(dni)) {
-        const clientData = await getClient(dni);
-        uploadClientTable(clientData[0]);
-        /* return clientData; */
-    }
+async function uploadAllClientsInfo() {
+    const clientData = await getAllClients();
+    agregarFilasATabla(clientData);
 }
 
 function verifyIsValidDni(e) {
-    if (e.length >= 7 && e.length <= 9 && !isNaN(e)) return true;
-    else {
-        /* invalidToaster({ code: "dni_incomplete" }); */
-        return false
-    }
+    if (e.length >= 5 && e.length <= 15 && !isNaN(e)) return true;
 }
 
-function uploadClientTable(client) {
-    rowForDni.innerHTML = client.dni;
-    rowForName.innerHTML = `${client.name}` + ` ` + `${client.surname}`
+function agregarFilasATabla(datos) {
+    const tabla = document.querySelector('table');
+    const tbody = tabla.querySelector('tbody');
+
+    datos.forEach((dato) => {
+        const fila = document.createElement('tr');
+        const dni = document.createElement('td');
+        const nombreCompleto = document.createElement('td');
+        const editarDatos = document.createElement('td');
+        const botonVisualizar = document.createElement('button');
+
+        dni.textContent = dato.dni;
+        nombreCompleto.textContent = `${dato.name}` + ` ` + `${dato.surname}`;
+
+        botonVisualizar.type = 'button';
+        botonVisualizar.className = 'btn btn-primary btn-lg';
+        botonVisualizar.textContent = 'Visualizar';
+
+        botonVisualizar.addEventListener('click', () => {
+            const modal = document.getElementById('modalVisualizar');
+
+            $(modal).modal('show');
+        });
+
+        editarDatos.appendChild(botonVisualizar);
+
+        fila.appendChild(dni);
+        fila.appendChild(nombreCompleto);
+        fila.appendChild(editarDatos);
+
+        tbody.appendChild(fila);
+    });
 }
 
 /**
@@ -38,3 +60,5 @@ form.addEventListener("submit", async function (e) {
         console.log(error);
     }
 });
+
+$(document).ready(uploadAllClientsInfo());
