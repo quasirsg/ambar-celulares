948e83f17791dddbfe8d6bfd36470473e722a1f2
Oscar Nicolas Chaves <70153563+Nicolas2021@users.noreply.github.com>
2025-01-11 00:27:44 -0300

    Feat/add-pagination-logic-and-change-styles-of-table

--- src/public/view/clientSearch/script.js
+++ src/public/view/clientSearch/script.js
@@ -4,10 +4,15 @@ const inputSearch = document.getElementById("search-input");
 const form = document.getElementById("search-form");
 
 let dni;
+const paginationState = {
+    currentPage: 1,
+    rowsPerPage: 5,
+};
 
-async function uploadAllClientsInfo() {
-    const clientData = await getAllClients();
+async function uploadAllClientsInfo(state) {
+    const clientData = await getAllClients(state.currentPage, state.rowsPerPage);
     agregarFilasATabla(clientData);
+    paginationSettingsToButtons(dni, paginationState);
 }
 
 function verifyIsValidDni(e) {
@@ -17,7 +22,7 @@ function verifyIsValidDni(e) {
 function agregarFilasATabla(datos) {
     const tabla = document.querySelector('table');
     const tbody = tabla.querySelector('tbody');
-
+    tbody.innerHTML = '';
     datos.forEach((dato) => {
         const fila = document.createElement('tr');
         const dni = document.createElement('td');
@@ -49,6 +54,65 @@ function agregarFilasATabla(datos) {
 }
 
 /**
+ * PAGINATION LOGIC
+ */
+async function paginationSettingsToButtons(dni, state, filter = {}) {
+    const countClients = await getTotalClients();
+    const countTotalClients = countClients[0].total;
+    const totalPages = Math.ceil(countTotalClients / state.rowsPerPage);
+    $(".page-link-current").text(state.currentPage);
+    updateButtonStates(state, totalPages);
+    await eventListenerPaginationButtons(dni, state, totalPages, filter);
+}
+/**
+ *  Handles the state of pagination buttons based on the current page and pagination
+ */
+function updateButtonStates(state, pagination) {
+    if (state.currentPage <= 1) {
+        $(".page-item-prev").addClass("disabled").removeClass("enabled");
+    } else {
+        $(".page-item-prev").addClass("enabled").removeClass("disabled");
+    }
+
+    if (state.currentPage >= pagination) {
+        $(".page-item-next").addClass("disabled").removeClass("enabled");
+    } else {
+        $(".page-item-next").addClass("enabled").removeClass("disabled");
+    }
+}
+
+/**
+ * Add logic to the dataTables pagination buttons and controll the events of the current page
+ */
+async function eventListenerPaginationButtons(dni, state, pagination, filter) {
+    $(".page-item-prev").off("click").on("click", async function (e) {
+        e.preventDefault();
+        if ($(this).hasClass("disabled")) {
+            console.log("El botón 'Previous' está deshabilitado, no se realizará la acción.");
+            return;
+        }
+        if (state.currentPage > 1) {
+            state.currentPage--;
+            await uploadAllClientsInfo(state);
+            updateButtonStates(state, pagination);
+        }
+    });
+
+    $(".page-item-next").off("click").on("click", async function (e) {
+        e.preventDefault();
+        if ($(this).hasClass("disabled")) {
+            console.log("El botón 'Next' está deshabilitado, no se realizará la acción.");
+            return;
+        }
+        if (state.currentPage < pagination) {
+            state.currentPage++;
+            await uploadAllClientsInfo(state);
+            updateButtonStates(state, pagination);
+        }
+    });
+}
+
+/**
  * Submit form
  */
 form.addEventListener("submit", async function (e) {
@@ -61,4 +125,4 @@ form.addEventListener("submit", async function (e) {
     }
 });
 
-$(document).ready(uploadAllClientsInfo());
+$(document).ready(uploadAllClientsInfo(paginationState));
